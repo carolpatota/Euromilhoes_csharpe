d2a9343c9821b5b4eedeadc6d973e341d0940126
Carolina Silva <160644328+carolpatota@users.noreply.github.com>
2024-10-16 15:20:44 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace _3935_Carolina_T02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpbox_1 = new System.Windows.Forms.GroupBox();
+            this.lbl_numero = new System.Windows.Forms.Label();
+            this.lbl_n1 = new System.Windows.Forms.Label();
+            this.lbl_n2 = new System.Windows.Forms.Label();
+            this.lbl_n3 = new System.Windows.Forms.Label();
+            this.lbl_n4 = new System.Windows.Forms.Label();
+            this.lbl_n5 = new System.Windows.Forms.Label();
+            this.lbl_estrelas = new System.Windows.Forms.Label();
+            this.lbl_e1 = new System.Windows.Forms.Label();
+            this.lbl_e2 = new System.Windows.Forms.Label();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.gpbox_1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbox_1
+            // 
+            this.gpbox_1.Controls.Add(this.btn_1);
+            this.gpbox_1.Controls.Add(this.lbl_e2);
+            this.gpbox_1.Controls.Add(this.lbl_e1);
+            this.gpbox_1.Controls.Add(this.lbl_estrelas);
+            this.gpbox_1.Controls.Add(this.lbl_n5);
+            this.gpbox_1.Controls.Add(this.lbl_n4);
+            this.gpbox_1.Controls.Add(this.lbl_n3);
+            this.gpbox_1.Controls.Add(this.lbl_n2);
+            this.gpbox_1.Controls.Add(this.lbl_n1);
+            this.gpbox_1.Controls.Add(this.lbl_numero);
+            this.gpbox_1.Location = new System.Drawing.Point(143, 61);
+            this.gpbox_1.Name = "gpbox_1";
+            this.gpbox_1.Size = new System.Drawing.Size(339, 211);
+            this.gpbox_1.TabIndex = 0;
+            this.gpbox_1.TabStop = false;
+            this.gpbox_1.Text = "EUROMILHOES";
+            // 
+            // lbl_numero
+            // 
+            this.lbl_numero.AutoSize = true;
+            this.lbl_numero.Location = new System.Drawing.Point(138, 50);
+            this.lbl_numero.Name = "lbl_numero";
+            this.lbl_numero.Size = new System.Drawing.Size(49, 13);
+            this.lbl_numero.TabIndex = 0;
+            this.lbl_numero.Text = "Numeros";
+            // 
+            // lbl_n1
+            // 
+            this.lbl_n1.AutoSize = true;
+            this.lbl_n1.Location = new System.Drawing.Point(39, 98);
+            this.lbl_n1.Name = "lbl_n1";
+            this.lbl_n1.Size = new System.Drawing.Size(0, 13);
+            this.lbl_n1.TabIndex = 1;
+            // 
+            // lbl_n2
+            // 
+            this.lbl_n2.AutoSize = true;
+            this.lbl_n2.Location = new System.Drawing.Point(90, 98);
+            this.lbl_n2.Name = "lbl_n2";
+            this.lbl_n2.Size = new System.Drawing.Size(0, 13);
+            this.lbl_n2.TabIndex = 2;
+            // 
+            // lbl_n3
+            // 
+            this.lbl_n3.AutoSize = true;
+            this.lbl_n3.Location = new System.Drawing.Point(152, 98);
+            this.lbl_n3.Name = "lbl_n3";
+            this.lbl_n3.Size = new System.Drawing.Size(0, 13);
+            this.lbl_n3.TabIndex = 3;
+            // 
+            // lbl_n4
+            // 
+            this.lbl_n4.AutoSize = true;
+            this.lbl_n4.Location = new System.Drawing.Point(213, 98);
+            this.lbl_n4.Name = "lbl_n4";
+            this.lbl_n4.Size = new System.Drawing.Size(0, 13);
+            this.lbl_n4.TabIndex = 4;
+            // 
+            // lbl_n5
+            // 
+            this.lbl_n5.AutoSize = true;
+            this.lbl_n5.Location = new System.Drawing.Point(264, 98);
+            this.lbl_n5.Name = "lbl_n5";
+            this.lbl_n5.Size = new System.Drawing.Size(0, 13);
+            this.lbl_n5.TabIndex = 5;
+            // 
+            // lbl_estrelas
+            // 
+            this.lbl_estrelas.AutoSize = true;
+            this.lbl_estrelas.Location = new System.Drawing.Point(138, 143);
+            this.lbl_estrelas.Name = "lbl_estrelas";
+            this.lbl_estrelas.Size = new System.Drawing.Size(44, 13);
+            this.lbl_estrelas.TabIndex = 6;
+            this.lbl_estrelas.Text = "Estrelas";
+            // 
+            // lbl_e1
+            // 
+            this.lbl_e1.AutoSize = true;
+            this.lbl_e1.Location = new System.Drawing.Point(100, 184);
+            this.lbl_e1.Name = "lbl_e1";
+            this.lbl_e1.Size = new System.Drawing.Size(0, 13);
+            this.lbl_e1.TabIndex = 7;
+            // 
+            // lbl_e2
+            // 
+            this.lbl_e2.AutoSize = true;
+            this.lbl_e2.Location = new System.Drawing.Point(176, 184);
+            this.lbl_e2.Name = "lbl_e2";
+            this.lbl_e2.Size = new System.Drawing.Size(0, 13);
+            this.lbl_e2.TabIndex = 8;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(258, 15);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(73, 35);
+            this.btn_1.TabIndex = 9;
+            this.btn_1.Text = "Jogar";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gpbox_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gpbox_1.ResumeLayout(false);
+            this.gpbox_1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbox_1;
+        private System.Windows.Forms.Label lbl_estrelas;
+        private System.Windows.Forms.Label lbl_n5;
+        private System.Windows.Forms.Label lbl_n4;
+        private System.Windows.Forms.Label lbl_n3;
+        private System.Windows.Forms.Label lbl_n2;
+        private System.Windows.Forms.Label lbl_n1;
+        private System.Windows.Forms.Label lbl_numero;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label lbl_e2;
+        private System.Windows.Forms.Label lbl_e1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3935_Carolina_T02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            Random euro = new Random();
+
+            int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, e1 = 0, e2 = 0;
+
+            n1 = euro.Next(1, 51);
+            do { n2 = euro.Next(1, 51); } while (n2 == n1);
+            do { n3 = euro.Next(1, 51); } while (n3 == n1 || n3 == n2);
+            do { n4 = euro.Next(1, 51); } while (n4 == n1 || n4 == n2 || n4 == n3);
+            do { n5 = euro.Next(1, 51); } while (n5 == n1 || n5 == n2 || n5 == n3 || n5 == n4);
+
+            e1 = euro.Next(1, 13);
+            do { e2 = euro.Next(1, 13); } while (e2 == e1);
+
+            //exibir nas labels
+            lbl_n1.Text = n1.ToString();
+            lbl_n2.Text = n2.ToString();
+            lbl_n3.Text = n3.ToString();
+            lbl_n4.Text = n4.ToString();
+            lbl_n5.Text = n5.ToString();
+            lbl_e1.Text = e1.ToString();
+            lbl_e2.Text = e2.ToString();
+        }
+    }
+}
